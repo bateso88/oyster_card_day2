3affa04e10d0efff82361ea2f0e816474e76a511
Anna Cavalla <anna.cavalla@gmail.com>
2021-01-12 15:29:31 +0000

    12. Adding journey history tests passing

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,5 +1,5 @@
 class OysterCard
-  attr_reader :balance, :entry_station
+  attr_reader :balance, :entry_station, :list_of_journeys
 
   BALANCE_LIMIT = 90
   MINIMUM_AMOUNT = 1
@@ -7,6 +7,7 @@ class OysterCard
   def initialize(balance = 0)
     @balance = balance
     @entry_station = nil
+    @list_of_journeys = []
   end
 
   def top_up(value)
@@ -21,10 +22,12 @@ class OysterCard
   def touch_in(station)
     fail "Need to have at least £#{MINIMUM_AMOUNT}." if balance < MINIMUM_AMOUNT
     @entry_station = station
+
   end
 
-  def touch_out
+  def touch_out(station)
     deduct(MINIMUM_AMOUNT)
+    @list_of_journeys << {entry_station: entry_station, exit_station: station}
     @entry_station = nil
   end
 
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -3,6 +3,8 @@ require 'oyster_card'
 describe OysterCard do
 
   let(:station1) { $station1 = "Highgate" }
+  let(:station2) { $station2 = "Archway" }
+
   context 'balance' do
     it 'should respond to :balance' do
       expect(OysterCard.new(0)).to respond_to :balance
@@ -17,20 +19,18 @@ describe OysterCard do
     end
   end
 
-  context 'top_up' do 
+  context 'top_up' do
     it 'should respond to top_up' do
       expect(subject).to respond_to :top_up
     end
 
     it 'should top_up balance by set value' do
-      card = OysterCard.new
-      card.top_up(10)
-      expect(card.balance).to eq 10
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
     end
 
     it 'should raise error if balance exceeds 90' do
-      card = OysterCard.new
-      expect { card.top_up(91) }.to raise_error "Cannot exceed £#{OysterCard::BALANCE_LIMIT} limit."
+      expect { subject.top_up(91) }.to raise_error "Cannot exceed £#{OysterCard::BALANCE_LIMIT} limit."
     end
   end
 
@@ -48,19 +48,19 @@ describe OysterCard do
     it 'in_journey? should be false after touch_out' do
       card = OysterCard.new(12)
       card.touch_in(station1)
-      card.touch_out
+      card.touch_out(station2)
       expect(card).not_to be_in_journey
     end
 
     it 'error should be raised when trying to touch_in with less than MINIMUM_AMOUNT' do
-      card = OysterCard.new
-      expect { card.touch_in(station1) }.to raise_error "Need to have at least £#{OysterCard::MINIMUM_AMOUNT}."
+      erro = "Need to have at least £#{OysterCard::MINIMUM_AMOUNT}."
+      expect { subject.touch_in(station1) }.to raise_error erro
     end
 
     it 'balance should be deducted by MINIMUM_AMOUNT after touch_out' do
       card = OysterCard.new(10)
       card.touch_in(station1)
-      expect { card.touch_out }.to change{ card.balance }.by(-OysterCard::MINIMUM_AMOUNT)
+      expect { card.touch_out(station2) }.to change{ card.balance }.by(-OysterCard::MINIMUM_AMOUNT)
     end
   end
 
@@ -73,7 +73,18 @@ describe OysterCard do
     it 'should return previous station' do
       card = OysterCard.new(10)
       card.touch_in(station1)
-      expect(card.touch_out).to eq nil
+      expect(card.touch_out(station2)).to eq nil
+    end
+    it 'should have an empty history by default' do
+      card =  OysterCard.new(10)
+      expect(card.list_of_journeys).to be_empty
+    end
+    it "adds a journey when journey complete" do
+      card = OysterCard.new(10)
+      card.touch_in(station1)
+      card.touch_out(station2)
+      arra = [{:entry_station => station1, :exit_station => station2}]
+      expect(card.list_of_journeys).to eq arra
     end
   end
 end
